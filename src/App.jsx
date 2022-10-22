f586031a7856bb0055a8c91e85b1a7e8accf4520
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2022-10-21 19:47:26 -0500

    quiero pasar

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/Page_Crud' element={<Lista/>} exact></Route>
-        <Route path='/Page_Crud/agregar' element={<Agregar/>} exact></Route>
-        <Route path='/Page_Crud/editar' element={<Editar/>} exact></Route>
+        <Route path='/Para_pasar' element={<Lista/>} exact></Route>
+        <Route path='/Para_pasar/agregar' element={<Agregar/>} exact></Route>
+        <Route path='/Para_pasar/editar' element={<Editar/>} exact></Route>
       </Routes>
       </BrowserRouter> 
       
